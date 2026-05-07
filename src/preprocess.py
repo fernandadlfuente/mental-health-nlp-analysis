17c4fe7be171d1b48a4938877546b893e8c31df5
fernandadlfuente <157684670+fernandadlfuente@users.noreply.github.com>
2026-05-07 16:17:02 -0400

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,6 +4,7 @@ def main():
   print("loading data....")
   print("cleaning text....")
   print("tokenizing....")
+  print("removing noise....")
   print("preprocessing complete")
 
 if __name__ == "__main__":
