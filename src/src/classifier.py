c3de9b67ddd2e547d0012ea00ca0f6b2a40c5f41
fernandadlfuente <157684670+fernandadlfuente@users.noreply.github.com>
2026-05-07 16:18:58 -0400

    Update classifier.py to include main function

--- src/src/classifier.py
+++ src/src/classifier.py
@@ -1,1 +1,11 @@
-print("file created")
+print("running text classification...")
+
+def main():
+  print("loading dataset...")
+  print("training classifier...")
+  print("evaluating model...")
+  print("vectorizing text...")
+  print("classification is complete...")
+
+if __name__ == "__main__":
+  main()
