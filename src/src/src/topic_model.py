5bfd1ac04156e1cee6602d1886630fa8e755dd4b
fernandadlfuente <157684670+fernandadlfuente@users.noreply.github.com>
2026-05-07 16:20:32 -0400

    Update topic_model.py to include topic modeling steps

--- src/src/src/topic_model.py
+++ src/src/src/topic_model.py
@@ -1,1 +1,10 @@
-print("file created")
+print("runnning topic modeling...")
+
+def main():
+  print("loading cleaned text...")
+  print("identifying themes...")
+  print("topic modeling complete")
+  print("extracting topics...")
+
+if __name__ == "__main__":
+  main()
